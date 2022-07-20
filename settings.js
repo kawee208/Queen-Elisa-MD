8a58cebda0bd4a9a236c444ce896ec9e3a525cff
ManulaMax <109348311+ManulaMax@users.noreply.github.com>
2022-07-20 17:18:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -46,7 +46,7 @@ global.sp = '🔵'
 // Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති `` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
 global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
 global.alive =👋Hi.. I'm Online Now 
-	`🎊Welcome All🎊
+	`🎊Welcome Guys🎊
 
 🌸Main Rules : 
 🚫Inbox not allowed 
